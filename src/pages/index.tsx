d71fdf6f8a0f8420cd517fca667f09a1ca1ac06d
Vldislav Khlebets <uladzislau.khliabets@protofire.io>
2023-03-17 12:29:30 +0300

    feat: minor update

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+      <iframe src="https://staging.widget.real-time-reserves.ledgerlens.io/v1/truecurrencies"  width='500px' height='616px'/>
+    </>
+  )
+}
